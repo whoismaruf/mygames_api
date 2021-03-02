59af6fb35fa1d64d1b1001132f40d6815a423283
whoismaruf <whoismaruf@outlook.com>
2021-03-02 12:45:05 +0600

    Initialize Django project with DRF

---
+++ core/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+# Create your models here.
+
+
+class Games(models.Model):
+    name = models.CharField(max_length=250, blank=False)
+    is_purchased = models.BooleanField(default=False)
+    added_date = models.DateTimeField(default=timezone.now())
+    gamer = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name, self.gamer
